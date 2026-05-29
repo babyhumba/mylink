6ce7958046e2741df60d6ec3cfa4a1aa1d1e9c92
Humba <humba@example.com>
2026-05-29 10:51:50 +0900

    chore: 구글 서치 콘솔 소유권 확인 코드 업데이트

--- my-profile/app/layout.tsx
+++ my-profile/app/layout.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
   title: "MyLink",
   description: "여러 개의 흩어진 링크를 단 하나의 직관적인 페이지로",
   verification: {
-    google: "0Gz_rdXl_CWJKcccO1Snt4SEC2q0cbZzo7NSBsQJ7SQ",
+    google: "pjhcQphsXIaM08flpW60QGjev8ifQFRYEcP6V1QyPEI",
   },
 };
 
